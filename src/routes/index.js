62763735ea1f74b75a2fbfef5fd16762ea0881ec
Victor Hugo <victor.aquino@ccc.ufcg.edu.br>
2021-04-08 13:42:05 -0300

    corrigindo bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const routes = require('express').Router();
 const singup = require('./singup');
 const login = require('./login');
 
-routes.use('/singup', singup);
+routes.use('/signup', singup);
 routes.use('/login', login);
 
 module.exports = routes;
