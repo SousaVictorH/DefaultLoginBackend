05ab724df8a06804ae793167ee84387fa5e13a5a
Victor Hugo <victor.aquino@ccc.ufcg.edu.br>
2021-04-14 14:04:08 -0300

    finalizando api

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,11 @@ const routes = require('express').Router();
 const singup = require('./singup');
 const login = require('./login');
 
+// RETURNS API VERSION
+routes.get('/', (req, res) => {
+    return res.send('API V 1.0');
+})
+
 routes.use('/signup', singup);
 routes.use('/login', login);
 
