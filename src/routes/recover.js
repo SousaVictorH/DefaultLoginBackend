bebf63490f4db9bc65d15d587df5f46e4b92f6f0
Victor Hugo <victor.aquino@ccc.ufcg.edu.br>
2021-04-17 12:11:03 -0300

    corrigindo bug

--- src/routes/recover.js
+++ src/routes/recover.js
@@ -1,10 +1,8 @@
 const routes = require('express').Router();
 
-const tokenAuth = require('../middlewares/tokenVerification');
-
 const { recovery, update } = require('../request-response-http/recover');
 
-routes.post('/', tokenAuth, recovery);
-routes.post('/update', tokenAuth, update);
+routes.post('/', recovery);
+routes.post('/update', update);
 
 module.exports = routes;
